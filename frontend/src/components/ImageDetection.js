426de18a78c6de416e214d552e725457ec38830c
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-10-21 23:47:37 +0530

    changed in Image detection component

--- frontend/src/components/ImageDetection.js
+++ frontend/src/components/ImageDetection.js
@@ -4,7 +4,8 @@ function ImageDetection(){
     return(
         <div className = "ImageDetection">
         <h1>Image Detection</h1>
-        //<img src="https://e3.365dm.com/24/04/1600x900/skynews-pothole-vehicle_6530094.jpg?20240422163557"/>
+        <img src="https://e3.365dm.com/24/04/1600x900/skynews-pothole-vehicle_6530094.jpg?20240422163557" alt="Pothole"/>
+        Hii
         </div>
     );
 
